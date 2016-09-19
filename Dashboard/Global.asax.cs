adb2d1e6550f039117a74f50d90b299eef15d99d
Lei <lyang@msrb.org>
2016-09-19 10:29:13 -0400

    Add web log to dashboard web site (LY)

--- Dashboard/Global.asax.cs
+++ Dashboard/Global.asax.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Diagnostics.CodeAnalysis;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Dashboard.Common;
 
 namespace Dashboard
 {
+    [ExcludeFromCodeCoverage]
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -16,6 +16,18 @@ namespace Dashboard
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            if (exception != null)
+            {
+                WebLoggers.ApplicationTrace.ErrorFormat("Exception from RTRSITOpsDashboard - {0}", exception.Message);
+            }
+
+            // Clear the error
+            Server.ClearError();
         }
     }
 }
